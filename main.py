3bedef73925ad4ae62cb4ac90a9e2243b0b2824c
yebin033 <leeyebin0330@naver.com>
2025-05-27 14:39:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,3 +29,5 @@ fig, ax = plt.subplots()
 sns.scatterplot(data=df, x="Longitude", y="Latitude", hue="Cluster", palette="tab10", ax=ax)
 plt.title("📍 클러스터링 결과")
 st.pyplot(fig)
+df = pd.read_csv("https://raw.githubusercontent.com/사용자이름/저장소이름/브랜치이름/Delivery.csv")
+
